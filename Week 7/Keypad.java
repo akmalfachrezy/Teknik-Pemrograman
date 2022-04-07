cb7d8e1e8b29c11bc9d049d861f8931249b7ae61
Akmal Faqih Fachrezy <93989001+akmalfachrezy@users.noreply.github.com>
2022-04-08 04:28:27 +0700

    Add files via upload

---
+++ Week 7/Keypad.java
@@ -0,0 +1,18 @@
+
+package week7;
+
+import java.util.Scanner;
+
+public class Keypad {
+	private Scanner input; // reads data from the command line
+                      
+	// no-argument constructor initializes the Scanner
+	public Keypad() {
+		input = new Scanner(System.in);    
+	} 
+
+	// return an integer value entered by user 
+	public int getInput() {
+		return input.nextInt(); // we assume that user enters an integer
+	} 
+} 
