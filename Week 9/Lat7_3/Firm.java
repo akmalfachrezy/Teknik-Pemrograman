1d23bca9e437be3fa4f8433706233259282256e8
Akmal Faqih Fachrezy <93989001+akmalfachrezy@users.noreply.github.com>
2022-04-10 21:51:25 +0700

    Add files via upload

---
+++ Week 9/Lat7_3/Firm.java
@@ -0,0 +1,11 @@
+package Lat7_3;
+
+public class Firm {
+    //--
+    //Creates a stadd of empolyees for a frm pays them.
+    //----
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
